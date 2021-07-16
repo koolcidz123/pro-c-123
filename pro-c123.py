bb935f562c3f4e69529e77ce263af8093ab38475
koolcidz123 <67968678+koolcidz123@users.noreply.github.com>
2021-07-16 19:52:46 +0530

    Update pro-c123.py

--- pro-c123.py
+++ pro-c123.py
@@ -3,31 +3,32 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from PIL import Image
 import PIL.ImageOps
-import os, ssl, time
 
-if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-    getattr(ssl, '_create_unverified_context', None)): 
-    ssl._create_default_https_context = ssl._create_unverified_
 
-X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+
+X = np.load('image.npz')['arr_0']
+y = pd.read_csv("labels.csv")["labels"]
 print(pd.Series(y).value_counts())
-classes = ['A', 'B', 'C','D', 'E','F', 'G', 'H', 'I', 'J']
+classes = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 nclasses = len(classes)
-
+f
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=9, train_size=7500, test_size=2500)
+
 X_train_scaled = X_train/255.0
 X_test_scaled = X_test/255.0
+
+
 clf = LogisticRegression(solver='saga', multi_class='multinomial').fit(X_train_scaled, y_train)
 
 y_pred = clf.predict(X_test_scaled)
 accuracy = accuracy_score(y_test, y_pred)
 print("The accuracy is :- ",accuracy)
+
 
 cap = cv2.VideoCapture(0)
 
@@ -43,13 +44,15 @@ while(True):
     bottom_right = (int(width / 2 + 56), int(height / 2 + 56))
     cv2.rectangle(gray, upper_left, bottom_right, (0, 255, 0), 2)
 
+    
     roi = gray[upper_left[1]:bottom_right[1], upper_left[0]:bottom_right[0]]
+
     
     im_pil = Image.fromarray(roi)
 
     image_bw = im_pil.convert('L')
     image_bw_resized = image_bw.resize((28,28), Image.ANTIALIAS)
-    
+
     image_bw_resized_inverted = PIL.ImageOps.invert(image_bw_resized)
     pixel_filter = 20
     min_pixel = np.percentile(image_bw_resized_inverted, pixel_filter)
@@ -60,11 +63,12 @@ while(True):
     test_pred = clf.predict(test_sample)
     print("Predicted class is: ", test_pred)
 
+    
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
   except Exception as e:
     pass
 
-    cap.release()
-    cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
